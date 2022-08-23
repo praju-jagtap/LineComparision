72bf01093d95778647eec4904ea23a801e32ef2d
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-08-23 19:29:38 +0530

    [Prajakta] Add: Check Greater than Or Less than

--- LineComparisionProblem/Program.cs
+++ LineComparisionProblem/Program.cs
@@ -7,13 +7,15 @@
             Comparision comparison = new Comparision(1,2,3,4);
             double line1 = comparison.CalculateLength();
             Console.WriteLine("Length of 1st Line: " +line1);
-            Comparision linecomparison = new Comparision(1,2,3,4);
+            Comparision linecomparison = new Comparision(5,7,8,9);
             double line2 = linecomparison.CalculateLength();
             Console.WriteLine("Length of 2nd Line: " + line2);
-            if (line1 == line2)
+            if (line1.CompareTo(line2) == 0)
                 Console.WriteLine("Line1 and Line 2 both are Equal");
+            if (line1.CompareTo(line2) > 0)
+                Console.WriteLine("Line1 is Greater than Line2");
             else
-                Console.WriteLine("Both Lines are not Equal");
+                Console.WriteLine("Line2 is Greater than Line 1");
         }
     }
 }
