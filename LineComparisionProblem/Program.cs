8524ce0e3096b4c475b711585c7bf5deaa3a3e0e
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-08-23 19:11:33 +0530

    [Prajakta] Add: Check Equality of Two Lines

--- LineComparisionProblem/Program.cs
+++ LineComparisionProblem/Program.cs
@@ -4,8 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Comparision comparison = new Comparision(3,4,8,9);
-            comparison.CalculateLength();
+            Comparision comparison = new Comparision(1,2,3,4);
+            double line1 = comparison.CalculateLength();
+            Console.WriteLine("Length of 1st Line: " +line1);
+            Comparision linecomparison = new Comparision(1,2,3,4);
+            double line2 = linecomparison.CalculateLength();
+            Console.WriteLine("Length of 2nd Line: " + line2);
+            if (line1 == line2)
+                Console.WriteLine("Line1 and Line 2 both are Equal");
+            else
+                Console.WriteLine("Both Lines are not Equal");
         }
     }
 }
