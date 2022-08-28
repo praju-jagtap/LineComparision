262cd6f328209f2e27f1472fd38f1a4ec2bd1399
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-08-28 20:16:53 +0530

    [Prajakta] Add: Using Java Function preferable Or Not

--- LineComparisionProblem/Program.cs
+++ LineComparisionProblem/Program.cs
@@ -7,15 +7,15 @@
             Comparision comparison = new Comparision(1,2,3,4);
             double line1 = comparison.CalculateLength();
             Console.WriteLine("Length of 1st Line: " +line1);
-            Comparision linecomparison = new Comparision(5,7,8,9);
+
+            Comparision linecomparison = new Comparision(1,2,3,4);
             double line2 = linecomparison.CalculateLength();
             Console.WriteLine("Length of 2nd Line: " + line2);
+
             if (line1.CompareTo(line2) == 0)
-                Console.WriteLine("Line1 and Line 2 both are Equal");
-            if (line1.CompareTo(line2) > 0)
-                Console.WriteLine("Line1 is Greater than Line2");
+                Console.WriteLine("Line1 and Line 2 Length are Preferable.");
             else
-                Console.WriteLine("Line2 is Greater than Line 1");
+                Console.WriteLine("Line1 and Line 2 Length are Not Preferable.");
         }
     }
 }
